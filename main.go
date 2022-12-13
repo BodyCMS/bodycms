587efe77c05da0f048d2a78b6aada00356581c9a
Hung Tran <hung.p.tran@opswat.com>
2022-12-13 11:19:17 +0700

    doc: update swagger doc

--- main.go
+++ main.go
@@ -21,17 +21,9 @@ func main() {
 	app.Get("/swagger/*", swagger.HandlerDefault) // default
 
 	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-		URL:         "http://example.com/doc.json",
-		DeepLinking: false,
-		// Expand ("list") or Collapse ("none") tag groups by default
+		URL:          "http://example.com/doc.json",
+		DeepLinking:  false,
 		DocExpansion: "none",
-		// Prefill OAuth ClientId on Authorize popup
-		OAuth: &swagger.OAuthConfig{
-			AppName:  "OAuth Provider",
-			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
-		},
-		// Ability to change OAuth2 redirect uri location
-		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
 	}))
 
 	app.Listen(":8080")
